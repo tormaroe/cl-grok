26cbb4ce3176ba3d87ea4b6cd6c37bf2c516de12
Torbjørn Marø <torbjorn.maro@gmail.com>
2017-02-05 20:01:11 +0100

    Refactored test script

--- test.lisp
+++ test.lisp
@@ -6,31 +6,26 @@
 ;;;           $ sbcl --load test.lisp --quit
 ;;;
 
+
 (format t "** RUNNING TESTS~%")
 
 (format t "*  LOADING CL-GROK~%")
 (ql:quickload :cl-grok)
 
+(defun test-match (&key text pattern dpl expected)
+  (let* ((filter (cl-grok:make-filter pattern dpl))
+         (match (funcall filter text)))
+    (assert (equal expected match) (match))))
+
 (format t "*  LOADING DEFAULT~%")
 (let ((default-patterns (cl-grok:load-default)))
 
-  (format t "*  ASSERTING A DEFAULT PATTERN~%")
-  (let ((IP-pattern (cl-grok:get-named-pattern "IP" default-patterns)))
-    (assert (equal "(?:%{IPV6}|%{IPV4})" IP-pattern) (IP-pattern))
+  (test-match :text "My name is tormaroe"
+              :pattern "^My name is %{USERNAME:name}"
+              :dpl default-patterns
+              :expected '(("name" . "tormaroe")))
 
-    (format t "*  MAKING A FILTER~%")
-    (let ((filter (cl-grok:make-filter "^My name is %{USERNAME:name}" 
-                                       default-patterns)))
 
-      ; TODO: Assert stuff about pattern
 
-      ; TODO: Run a match using filter
-      (let ((match (funcall filter "My name is tormaroe")))
-
-        ; TODO: Assert the result 
-        (assert (equal "tormaroe" (cdr (assoc "name" match :test #'equal)))
-                (match)))
-
-  )))
-
+  )
 (format t "** END OF TESTS~%")
